a9debfe34fa31057a377fb844cc7367c6934523c
acaruso1030 <a.caruso@vcinsurancemail.com>
2025-08-06 00:25:19 -0400

    Fully removed blog assets and broken imports

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -84,8 +84,8 @@ const Footer = () => {
                 <Link href="/#pricing" className="link link-hover">
                   Pricing
                 </Link>
-                <Link href="/blog" className="link link-hover">
-                  Blog
+              
+                
                 </Link>
                 <a href="/#" target="_blank" className="link link-hover">
                   Affiliates
